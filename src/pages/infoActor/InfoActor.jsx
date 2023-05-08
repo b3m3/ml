130e50ceed4767cc0db47807ade93895db0064a4
b3m3 <dtstht@gmail.com>
2023-05-08 15:36:12 +0300

    cr InfoActor p

---
+++ src/pages/infoActor/InfoActor.jsx
@@ -0,0 +1,11 @@
+import style from './info-actor.module.scss';
+
+const InfoActor = () => {
+  return (
+    <div className={style.wrapp}>
+      Actor Info
+    </div>
+  );
+}
+
+export default InfoActor;
