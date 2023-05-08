76af7b4fadbd4248efc419e02b5022b96a2638a4
b3m3 <dtstht@gmail.com>
2023-05-08 15:36:28 +0300

    cr InfoTvShows p

---
+++ src/pages/infoTvShows/InfoTvShows.jsx
@@ -0,0 +1,11 @@
+import style from './info-tv-shows.module.scss';
+
+const InfoTvShows = () => {
+  return (
+    <div className={style.wrapp}>
+      InfoTvShows
+    </div>
+  );
+}
+
+export default InfoTvShows;
