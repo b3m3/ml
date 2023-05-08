b8087b75b123597c7239c0e3d02b5e88ebf07f7a
b3m3 <dtstht@gmail.com>
2023-05-08 15:45:22 +0300

    Recommendations cp

---
+++ src/components/recommendations/Recommendations.jsx
@@ -0,0 +1,41 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import CardVideo from '../cardVideo/CardVideo';
+import { fetchRecomendations } from '../../store/slices/fetchDataSlice';
+
+import style from './recommendations.module.scss';
+
+const Recommendations = ({ type, id }) => {
+  const dispatch = useDispatch();
+  const { recommendations } = useSelector(state => state.fetchData);
+
+  useEffect(() => {
+    dispatch(fetchRecomendations({ type, id }));
+  }, [dispatch, type, id]);
+
+  return (
+    <>
+      {
+        Boolean(recommendations?.res?.results.length) &&
+          <div className={style.wrapp}>
+            <h5>Recommendations</h5>
+
+            <ul>
+              {
+                recommendations?.res?.results.slice(0, 8).map(props => {
+                  return (
+                    <li key={props.id}>
+                      <CardVideo {...props} noHover />
+                    </li>
+                  )
+                })
+              }
+            </ul>
+          </div>
+      }
+    </>
+  );
+}
+
+export default Recommendations;
