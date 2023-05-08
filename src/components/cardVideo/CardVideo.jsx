7213f4bb3f412b8d8629aa62ee84dc1f95241743
b3m3 <dtstht@gmail.com>
2023-05-08 15:43:53 +0300

    CardVideo cp

---
+++ src/components/cardVideo/CardVideo.jsx
@@ -0,0 +1,50 @@
+import { Link } from 'react-router-dom';
+import Poster from '../poster/Poster';
+
+import { getTypeFromPathname } from '../../utils/functions';
+import { TbUnlink } from 'react-icons/tb';
+import { AiFillStar } from 'react-icons/ai';
+
+import style from './card-video.module.scss';
+
+const CardVideo = ({ id, poster_path, backdrop_path, release_date, first_air_date, title, name, vote_average, noHover, wide }) => {
+  const type = getTypeFromPathname();
+
+  return (
+    <div className={style.wrapp}>
+      <Link 
+        to={`/${type}/${id}`}
+        className={`${style.top} ${noHover && style.no_hover}`}
+        style={wide ? {padding: '25% 0'} : null} 
+      >
+        <Poster url={wide ? backdrop_path : poster_path} size={'w500'}/>
+        <TbUnlink />
+      </Link>
+
+      <div className={style.body}>
+        <p>{title ? title : name}</p>
+
+        <div className={style.body__row}>
+          {
+            !noHover && vote_average &&
+              <p>
+                <AiFillStar style={{color: "orange"}} />
+                {vote_average}
+              </p>
+          }
+          <p>
+            { 
+              !noHover && release_date
+                ? release_date.slice(0, 4) 
+                : first_air_date 
+                ? first_air_date.slice(0, 4)
+                : null
+            }
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CardVideo;
