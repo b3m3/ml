6b85a9ac445167ef18f6708698b3b79385300999
b3m3 <dtstht@gmail.com>
2023-05-08 15:44:40 +0300

    PageNavigation cp

---
+++ src/components/pageNavigation/PageNavigation.jsx
@@ -0,0 +1,60 @@
+import { Link } from 'react-router-dom';
+import { getTypeFromPathname } from '../../utils/functions';
+
+import style from './page-navigation.module.scss';
+
+const PageNavigation = ({ totalPages, currentPage, category }) => {
+  const type = getTypeFromPathname();
+
+  const total = totalPages && totalPages > 500 ? 500 : totalPages;
+
+  const link = `/${type}/${category}/`;
+  const activeStyle = {color: 'var(--blue-400)'};
+
+  return (
+    <>
+      {
+        total && total > 1 &&
+        <div className={style.wrapp}>
+          <ul>
+            <li style={currentPage === 1 ? activeStyle : null}> 
+              <Link to={link + 1}>{1}</Link> 
+            </li>
+            <li>...</li>
+            {currentPage > 3 && 
+              <li >
+                <Link to={`${link}${currentPage - 2}`}>{currentPage - 2}</Link>
+              </li>
+            }
+            {currentPage > 2 && 
+              <li >
+                <Link to={`${link}${currentPage - 1}`}>{currentPage - 1}</Link>
+              </li>
+            }
+            {currentPage > 1 && currentPage < total &&
+              <li style={activeStyle}>
+                <Link to={`${link}${currentPage}`}>{currentPage}</Link>
+              </li>
+            }
+            {currentPage + 1 < total && 
+              <li >
+                <Link to={`${link}${currentPage + 1}`}>{currentPage + 1}</Link>
+              </li>
+            }
+            {currentPage + 2 < total &&
+              <li >
+                <Link to={`${link}${currentPage + 2}`}>{ currentPage + 2}</Link>
+              </li>
+            }
+            <li>...</li>
+            <li style={currentPage === total ? activeStyle : null}>
+              <Link to={link + total}>{total}</Link>
+            </li>
+          </ul>
+        </div>
+      }
+    </>
+  );
+}
+
+export default PageNavigation;
