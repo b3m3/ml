c6ed8b95886d2068008f48be01dd359dfa63d669
b3m3 <dtstht@gmail.com>
2023-05-18 14:19:26 +0300

    rem fetch info

--- src/pages/home/upcoming/cardUpcoming/CardUpcoming.jsx
+++ src/pages/home/upcoming/cardUpcoming/CardUpcoming.jsx
@@ -1,21 +1,10 @@
-import { useEffect, useState } from 'react';
-import axios from '../../../../utils/axios';
-
 import { Link } from 'react-router-dom';
+
 import Poster from '../../../../components/poster/Poster';
+
 import style from './card-upcoming.module.scss';
 
 const CardUpcoming = ({ id, poster_path, title }) => {
-  const [results, setResults] = useState(null);
-
-  useEffect(() => {
-    if (id) {
-      axios.get(`/movie/${id}?api_key=${process.env.REACT_APP_API_KEY}&language=en`)
-        .then(data => setResults(data.data))
-        .catch(err => console.log(err));
-    }
-  }, [id]);
-
   return (
     <Link 
       className={style.wrapp}
@@ -25,29 +14,9 @@ const CardUpcoming = ({ id, poster_path, title }) => {
         <Poster url={poster_path} size={'w300'} />
       </div>
 
-      {
-        results?.runtime &&
-          <span className={style.time}>{results?.runtime} min</span>
-      }
-
       <div className={style.body}>
         <h5>Movie</h5>
         <h3>{title && title}</h3>
-
-        {
-          results?.genres &&
-            <ul>
-              {
-                results?.genres?.slice(0, 3).map(({id, name}) => {
-                  return (
-                    <li key={id}>
-                      {name}
-                    </li>
-                  )
-                })
-              }
-            </ul>
-        }
       </div>
     </Link>
   );
