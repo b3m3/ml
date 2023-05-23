fc75975d066f5c61afcc5a0de1b06cb2d6cb0afb
b3m3 <dtstht@gmail.com>
2023-05-23 13:35:28 +0300

    upd currentTime

--- src/components/header/currentTime/CurrentTime.jsx
+++ src/components/header/currentTime/CurrentTime.jsx
@@ -1,26 +1,53 @@
+import { useEffect, useState, memo } from 'react';
+import { addZero } from '../../../utils/functions';
+
 import style from './current-time.module.scss';
 
-// const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-// const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-const CurrentTime = () => {
+const CurrentTime = memo(() => {
+  const [hours, setHours] = useState('');
+  const [minutes, setMinutes] = useState('');
+  const [seconds, setSeconds] = useState('');
+  
+  const date = new Date();
+
+  const weekDay = date.getDay();
+  const day = date.getDate();
+  const month = date.getMonth();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const h = date.getHours();
+      const m = date.getMinutes();
+      const s = date.getSeconds();
+
+      setHours(h);
+      setMinutes(m);
+      setSeconds(s);
+    }, 1000)
+
+    return () =>  clearInterval(interval);
+  }, [date, seconds]);
+
   return (
     <div className={style.wrapp}>
       <div className={style.time}>
-        <span>09</span>
+        <span>{ hours ? addZero(hours) : '--' }</span>
         <span>:</span>
-        <span>27</span>
+        <span>{ minutes ? addZero(minutes) : '--' }</span>
       </div>
       <div className={style.date}>
         <div className={style.date__row}>
-          <span>April</span>
-          <span>05</span>
+          <span>{ months[month] }</span>
+          <span>{ addZero(day) }</span>
         </div>
 
-        <p className={style.date__day}>Wednesday</p>
+        <p className={style.date__day}>{ days[weekDay] }</p>
       </div>
     </div>
   );
-}
+});
 
 export default CurrentTime;
