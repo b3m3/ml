690e206c30d8196bc3dff467f334eb1e75fcc944
b3m3 <dtstht@gmail.com>
2023-05-24 17:14:14 +0300

    Favorites list

--- src/pages/favorites/Favorites.jsx
+++ src/pages/favorites/Favorites.jsx
@@ -1,9 +1,50 @@
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+
+import CardVideo from '../../components/cardVideo/CardVideo';
+import Select from './select/Select';
+import Loading from '../../components/loading/Loading';
+import Error from '../../components/error/Error';
+
 import style from './favorites.module.scss';
 
 const Favorites = () => {
+  const [currentType, setCurrentType] = useState('');
+  const { favoriteMovies, favoriteTv } = useSelector(state => state.favorite);
+
+  const favoritelist = currentType === 'Movies' ? favoriteMovies : favoriteTv;
+  const media_type= currentType === 'Movies' ? 'movie' : 'tv';
+
   return (
-    <div className={style.wrapp}>
-      Favorites
+    <div className="container">
+      <div className={style.wrapp}>
+        <div className={style.top}>
+          <Select currentType={currentType} setCurrentType={setCurrentType} />
+        </div>
+
+        {
+          favoritelist.res &&
+            <ul className={style.body}>
+              {
+                favoritelist.res?.map(props => {
+                  return (
+                    <li key={props.id}>
+                      <CardVideo {...props} media_type={media_type} />
+                    </li>
+                  )
+                })
+              }
+            </ul>
+        }
+        
+        { 
+          favoritelist.status && <Error status={favoritelist.status.message} page />
+        }
+
+        {
+          favoritelist.loading && <Loading />
+        }
+      </div>
     </div>
   );
 }
